7e7370fee55131017e9c6e986e227bbb871deda1
LUIGI BARBIERI LOMBARDO <luigi_lombardo@estudante.sesisenai.org.br>
2026-09-14 15:37:15 -0300

    first commit

---
+++ src/main/java/br/com/ctw/monitoramento_transformadores/MonitoramentoTransformadoresApplication.java
@@ -0,0 +1,13 @@
+package br.com.ctw.monitoramento_transformadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoramentoTransformadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoramentoTransformadoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ctw/monitoramento_transformadores/MonitoramentoTransformadoresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ctw.monitoramento_transformadores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoramentoTransformadoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
